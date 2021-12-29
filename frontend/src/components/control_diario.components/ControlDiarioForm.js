531fdfa5ff9181ca5845a14a4db7433f0db9ff38
Lapsa09 <agustin.dinardo@vicentelopez.gov.ar>
2021-12-29 16:30:40 -0300

    fix date time

--- frontend/src/components/control_diario.components/ControlDiarioForm.js
+++ frontend/src/components/control_diario.components/ControlDiarioForm.js
@@ -137,7 +137,7 @@ function ControlDiarioForm({ handleClose, afterCreate }) {
   };
 
   const parseDate = (newDate) => {
-    if (!newDate.isInvalid()) {
+    if (newDate.isValid) {
       setDate(newDate);
       setForm({ ...form, fecha: newDate.toLocaleString() });
     } else {
@@ -146,7 +146,7 @@ function ControlDiarioForm({ handleClose, afterCreate }) {
   };
 
   const parseTime = (newTime) => {
-    if (!newTime.isInvalid()) {
+    if (newTime.isValid) {
       setTime(newTime);
       setForm({
         ...form,

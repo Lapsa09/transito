d73f8eae3ef21e731a8c1e54a1ba60a046fb8560
Lapsa09 <agustin.dinardo@vicentelopez.gov.ar>
2022-01-03 16:53:48 -0300

    logout

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,14 +1,21 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import { Logout } from "@mui/icons-material";
 
 function App() {
+  const navigate = useNavigate();
+  const logout = () => {
+    localStorage.removeItem("token");
+    navigate(0);
+  };
   return (
-    <div>
+    <div className="home">
       <h1>Home</h1>
       <nav>
         <NavLink to="/">Home</NavLink>
         <NavLink to="/accidentes">Accidentes</NavLink>
         <NavLink to="/operativos">Operativos</NavLink>
         <NavLink to="/control">Control Diario</NavLink>
+        <Logout onClick={logout} />
       </nav>
     </div>
   );

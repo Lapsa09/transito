9db0bdf61d6dd177e8f02eb1ab6753b6428b4c2d
AGUSTIN DI NARDO <agustin.dinardo@vicentelopez.gov.ar>
2023-03-07 14:52:00 -0300

    route fix

--- src/layouts/PrivateRoute.tsx
+++ src/layouts/PrivateRoute.tsx
@@ -1,4 +1,4 @@
-import { IRootState } from '@redux/store'
+import { IRootState } from '../redux'
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Navigate } from 'react-router-dom'

f5664c3c855d94286ed871fa592be6c47f17187d
Lapsa09 <agustin.dinardo@vicentelopez.gov.ar>
2023-09-08 10:19:09 -0300

    strange error in api deleted

--- services/main.service.ts
+++ services/main.service.ts
@@ -82,14 +82,6 @@ export const getMotivos = async () => {
   })
   return data
 }
-
-export const getRoles = async () => {
-  const data = await getter<{ [permiso: string]: string }>({
-    route: 'roles',
-  })
-  return data
-}
-
 export const getZonasPaseo = async () => {
   const data = await getter<zonas[]>({
     route: 'zonas/paseo',

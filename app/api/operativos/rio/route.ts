b55df3781d64e54ddddee54e8604f1144e680ad7
Lapsa09 <agusdinar96@gmail.com>
2023-10-07 19:14:48 -0300

    radicacion now done on the same server, no need for python anymore

--- app/api/operativos/rio/route.ts
+++ app/api/operativos/rio/route.ts
@@ -1,10 +1,9 @@
 import prisma from '@/lib/prismadb'
 import { RioFormProps } from '@/types'
 import { NextRequest, NextResponse } from 'next/server'
-import { spawn } from 'child_process'
-import { once } from 'events'
 import { DateTime } from 'luxon'
 import { turnos } from '@prisma/client'
+import { load } from 'cheerio'
 
 const operativoPaseo = async (body: RioFormProps) => {
   const { fecha, turno, lp } = body
@@ -37,57 +36,74 @@ const operativoPaseo = async (body: RioFormProps) => {
 
 const radicacion = async (body: RioFormProps) => {
   try {
-    const res: number[] = []
     const { dominio } = body
 
-    const pythonProcess = spawn('python', [
-      process.env.NEXT_PUBLIC_DNRPA_ROUTE!,
-      dominio,
-    ])
-    pythonProcess.stdout.on('data', async (data) => {
-      const decodedData = data.toString()
+    const data = await fetch(
+      'https://www.dnrpa.gov.ar/portal_dnrpa/radicacion/consinve_amq.php',
+      {
+        headers: {
+          accept:
+            'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+          'accept-language': 'es-ES,es;q=0.9',
+          'cache-control': 'max-age=0',
+          'content-type': 'application/x-www-form-urlencoded',
+          'sec-ch-ua':
+            '"Chromium";v="116", "Not)A;Brand";v="24", "Opera";v="102"',
+          'sec-ch-ua-mobile': '?0',
+          'sec-ch-ua-platform': '"Windows"',
+          'sec-fetch-dest': 'document',
+          'sec-fetch-mode': 'navigate',
+          'sec-fetch-site': 'same-origin',
+          'sec-fetch-user': '?1',
+          'upgrade-insecure-requests': '1',
+        },
+        referrer: 'https://www.dnrpa.gov.ar/portal_dnrpa/radicacion2.php',
+        referrerPolicy: 'strict-origin-when-cross-origin',
+        body: `dominio=${dominio}`,
+        method: 'POST',
+        mode: 'cors',
+        credentials: 'omit',
+      },
+    )
 
-      const _data = JSON.parse(decodedData.replace(/'/g, '"'))
+    const html = await data.text()
+    const $ = load(html)
 
-      const { provincia, localidad } = _data
-      if (provincia !== 'BUENOS AIRES' && provincia !== 'CAPITAL FEDERAL') {
+    const secs = $('table').eq(3)
+    const fonts = secs.find('font')
+    const localidad = fonts.eq(1).text().trim()
+    const provincia = fonts.eq(3).text().trim()
+
+    if (provincia !== 'BUENOS AIRES' && provincia !== 'CAPITAL FEDERAL') {
+      const _res = await prisma.barrios.findFirst({
+        where: {
+          barrio: provincia,
+        },
+      })
+      return _res!.id_barrio
+    } else {
+      if (localidad === 'CAPITAL FEDERAL') {
+        return 51
+      } else {
         const _res = await prisma.barrios.findFirst({
           where: {
-            barrio: provincia,
+            barrio: {
+              contains: localidad,
+            },
           },
         })
-        res.push(_res!.id_barrio)
-      } else {
-        if (localidad === 'CAPITAL FEDERAL') {
-          res.push(51)
-        } else {
-          const _res = await prisma.barrios.findFirst({
-            where: {
-              barrio: {
-                contains: localidad,
-              },
+        if (!_res) {
+          const nuevoBarrio = await prisma.barrios.create({
+            data: {
+              barrio: localidad,
             },
           })
-          if (!_res) {
-            const nuevoBarrio = await prisma.barrios.create({
-              data: {
-                barrio: localidad,
-              },
-            })
-            res.push(nuevoBarrio.id_barrio)
-          } else {
-            res.push(_res.id_barrio)
-          }
+          return nuevoBarrio.id_barrio
+        } else {
+          return _res.id_barrio
         }
       }
-    })
-    pythonProcess.stdout.on('error', (error) => {
-      console.log(error.message)
-      return NextResponse.json('El dominio no existe', { status: 400 })
-    })
-
-    await once(pythonProcess, 'close')
-    return res
+    }
   } catch (error) {
     console.log(error)
     throw new Error('Server error')
@@ -124,7 +140,7 @@ export async function POST(req: NextRequest) {
     return NextResponse.json('El dominio ya fue cargado', { status: 400 })
   }
 
-  const [id_localidad] = await radicacion(data)
+  const id_localidad = await radicacion(data)
 
   const _hora = new Date(data.fecha)
   // @ts-ignore

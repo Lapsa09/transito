0b8d6b3570e580aaf513bc17bf1102c685935269
Lapsa09 <agustin.dinardo@vicentelopez.gov.ar>
2023-11-01 10:00:12 -0300

    fixed logout for all roles

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,32 +1,12 @@
-'use client'
-
 import React from 'react'
 import { LogoOVT, LogoVL } from '../Logos'
 import Menu from './Menu'
-import { useSession } from 'next-auth/react'
-import { Roles } from '@/types'
 
 function Header() {
-  const session = useSession()
-
-  if (session.status === 'unauthenticated' || session.status === 'loading')
-    return null
-
-  const user = session.data?.user
-
-  const fullName = user?.nombre + ' ' + user?.apellido
   return (
     <nav className="border-gray-200 bg-white px-8 flex flex-wrap items-center justify-between shadow-[0_0_21px_-7px_rgba(0,0,0,0.41)] mb-4 sticky top-0 z-50">
       <LogoVL link />
-      {user?.role === Roles.ADMIN ? (
-        <Menu />
-      ) : (
-        <div className="flex justify-between">
-          <h3 className="capitalize">
-            {fullName} Legajo {user?.legajo}
-          </h3>
-        </div>
-      )}
+      <Menu />
       <LogoOVT className="lg:order-3" />
     </nav>
   )
--- components/header/Menu.tsx
+++ components/header/Menu.tsx
@@ -4,7 +4,7 @@ import { Links, Roles } from '@/types'
 import Link from 'next/link'
 import MenuButton from './MenuButton'
 import { FiLogOut } from 'react-icons/fi'
-import { signOut } from 'next-auth/react'
+import { signOut, useSession } from 'next-auth/react'
 import { Button, Typography } from '@material-tailwind/react'
 import Dropdown from './Dropdown'
 
@@ -23,7 +23,7 @@ const pages: Links[] = [
     permission: Roles.INSPECTOR,
   },
   { name: 'Sueldos', permission: Roles.ADMINISTRATIVO, link: '/sueldos' },
-  // { name: 'Waze', permission: Roles.WAZE, link: '/waze' },
+  { name: 'Waze', permission: Roles.WAZE, link: '/waze' },
   // { name: 'Radio', permission: Roles.INSPECTOR, link: '/radio' },
   { name: 'Logistica', permission: Roles.LOGISTICA, link: '/logistica' },
 ]
@@ -32,27 +32,42 @@ function Menu() {
   const logout = () => {
     signOut({ callbackUrl: '/login' })
   }
+  const session = useSession()
+
+  if (session.status === 'unauthenticated' || session.status === 'loading')
+    return null
+
+  const user = session.data?.user
+
+  const fullName = user?.nombre + ' ' + user?.apellido
   return (
     <>
       <MenuButton />
       <div className="hidden lg:block w-full md:w-1/2">
         <ul className="flex-col md:flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-md md:font-medium md:justify-between items-center">
-          {pages.map((page) =>
-            page.links ? (
-              <Dropdown key={page.name} page={page} />
-            ) : (
-              <Link key={page.name} href={page.link}>
-                <Button
-                  variant="text"
-                  key={page.name}
-                  className="text-gray-700 md:hover:text-green-400 capitalize text-base font-medium"
-                >
-                  {page.name}
-                </Button>
-              </Link>
-            ),
+          {user?.role === Roles.ADMIN ? (
+            pages.map((page) =>
+              page.links ? (
+                <Dropdown key={page.name} page={page} />
+              ) : (
+                <Link key={page.name} href={page.link}>
+                  <Button
+                    variant="text"
+                    key={page.name}
+                    className="text-gray-700 md:hover:text-green-400 capitalize text-base font-medium"
+                  >
+                    {page.name}
+                  </Button>
+                </Link>
+              ),
+            )
+          ) : (
+            <div className="flex justify-between">
+              <h3 className="capitalize">
+                {fullName} Legajo {user?.legajo}
+              </h3>
+            </div>
           )}
-
           <Button variant="text" className="text-gray-700 hover:text-green-400">
             <Typography className="px-3 py-2 md:hidden">
               Cerrar sesion

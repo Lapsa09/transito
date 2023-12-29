c72e59e3eea2ac9f0e5397b2f2ae4d643e779f5c
Lapsa09 <agustin.dinardo@vicentelopez.gov.ar>
2023-12-29 16:21:02 -0300

    ready for pr

--- services/auth.service.ts
+++ services/auth.service.ts
@@ -1,8 +1,8 @@
-import { LoginUserProps, RegisterUserProps, User } from '@/types'
+import { LoginUserProps, RegisterUserProps, RegularUser, User } from '@/types'
 import { setter } from './main.service'
 
 export const signUp = async (body: RegisterUserProps) =>
   await setter<User>({ route: 'register', body })
 
 export const signIn = async (body: LoginUserProps) =>
-  await setter<User | null>({ route: 'login', body })
+  await setter<RegularUser | null>({ route: 'login', body })
--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,12 +1,17 @@
-import { Roles, User as IUser } from './Misc'
+import { DefaultSession } from 'next-auth'
+import { Roles, User as IUser, RegularUser, InvitedUser } from './Misc'
 
 declare module 'next-auth' {
   interface User extends IUser {}
   interface Session extends DefaultSession {
-    user?: IUser
+    user?: RegularUser
+  }
+  interface Session extends DefaultSession {
+    user?: InvitedUser
   }
 }
 
 declare module 'next-auth/jwt' {
-  interface JWT extends IUser {}
+  interface JWT extends RegularUser {}
+  interface JWT extends InvitedUser {}
 }

4d6f27739f946eeaf09adc08c7aedee750d241a3
Lapsa09 <agustin.dinardo@vicentelopez.gov.ar>
2024-01-19 14:51:04 -0300

    added properties missing

--- app/sueldos/(subpages)/servicios/edit/[id]/page.tsx
+++ app/sueldos/(subpages)/servicios/edit/[id]/page.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import { EditFormLayout } from '@/components/forms/layout.form'
 import LayoutServiciosForm from '@/components/forms/servicios.form'
+import { getPrecios, getSelects } from '@/services'
 
-function page() {
+async function page() {
+  const { clientes, operarios } = await getSelects()
+  const precios = await getPrecios()
   return (
     <EditFormLayout
       className="flex flex-col justify-center px-6"
       section="sueldos"
       stepTitles={['Editar servicio']}
     >
-      <LayoutServiciosForm />
+      <LayoutServiciosForm
+        selects={{ clientes, operarios }}
+        precios={precios}
+      />
     </EditFormLayout>
   )
 }

b0514ece95b815ae7e01200aa1ed3e6a418945f7
Lapsa09 <agustin.dinardo@vicentelopez.gov.ar>
2024-02-21 12:59:27 -0300

    logistica layout is now server component

---
+++ app/logistica/(lists)/layout.client.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import Link from 'next/link'
+import { useSelectedLayoutSegments } from 'next/navigation'
+import React, { PropsWithChildren } from 'react'
+import { useRouter } from 'next/navigation'
+
+function LayoutClient({ children }: PropsWithChildren) {
+  const title = useSelectedLayoutSegments()
+  const router = useRouter()
+  return (
+    <div>
+      <h1 className="text-center mb-5 capitalize">{title.at(-1)}</h1>
+      <h1 className="cursor-pointer w-fit" onClick={router.back}>
+        Atras
+      </h1>
+      {!(title[0].includes('repuestos') && title.length === 1) && (
+        <Link href={`/logistica/${title.join('/')}/create`}>
+          Nuevo {title.at(-1)}
+        </Link>
+      )}
+      {children}
+    </div>
+  )
+}
+
+export default LayoutClient
--- app/logistica/(lists)/layout.tsx
+++ app/logistica/(lists)/layout.tsx
@@ -1,27 +1,8 @@
-'use client'
-
-import Link from 'next/link'
-import { useSelectedLayoutSegments } from 'next/navigation'
 import React, { PropsWithChildren } from 'react'
-import { useRouter } from 'next/navigation'
+import LayoutClient from './layout.client'
 
 function layout({ children }: PropsWithChildren) {
-  const title = useSelectedLayoutSegments()
-  const router = useRouter()
-  return (
-    <div>
-      <h1 className="text-center mb-5 capitalize">{title.at(-1)}</h1>
-      <h1 className="cursor-pointer w-fit" onClick={router.back}>
-        Atras
-      </h1>
-      {!(title[0].includes('repuestos') && title.length === 1) && (
-        <Link href={`/logistica/${title.join('/')}/create`}>
-          Nuevo {title.at(-1)}
-        </Link>
-      )}
-      {children}
-    </div>
-  )
+  return <LayoutClient>{children}</LayoutClient>
 }
 
 export default layout

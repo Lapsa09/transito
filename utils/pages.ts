3c8e3ea25397e0d2552b7676ed748d63c10290ce
Lapsa09 <agusdinar96@gmail.com>
2024-07-24 14:46:49 -0300

    partes fixed

--- utils/pages.ts
+++ utils/pages.ts
@@ -24,4 +24,10 @@ export const pages: Links[] = [
   },
   { name: 'Sueldos', permission: Roles.ADMINISTRATIVO, link: '/sueldos' },
   { name: 'Logistica', permission: Roles.LOGISTICA, link: '/logistica' },
+  {
+    name: 'Radio',
+    permission: Roles.RADIO,
+    link:
+      '/radio/partes?filter=fecha=' + new Date().toISOString().split('T')[0],
+  },
 ]
